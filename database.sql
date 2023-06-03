172b37b4dc70bc8d632094d75bc6e7d75788d306
Chris Clark <xchrisck@gmail.com>
2023-06-03 09:35:45 -0500

    created the sql database and updated the database.sql file

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE gallery (
+	id SERIAL PRIMARY KEY,
+	path VARCHAR(500) NOT NULL,
+	description VARCHAR (100), 
+	likes INT DEFAULT 0
+);
+
+
+INSERT INTO gallery (path, description)
+VALUES ('images/little_bird.jpeg', 'A fun little bird.'),
+('images/download.jpeg', 'A nice little valley view'),
+('images/download.png', 'Beautiful sunset'),
+('images/nighttime.jpeg', 'Gorgious night time camping view'),
+('images/sunflower.jpeg', 'Wonderful sunflower field'),
+('images/goat_small.jpg', 'Photo of a goat taken at Glacier National Park.');
