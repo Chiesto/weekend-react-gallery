7f7b8f476f1fa184a5e2aea8ecb7e3fc2b8d09eb
Chris Clark <xchrisck@gmail.com>
2023-06-03 09:54:40 -0500

    added the pool and updated the put and get routers to request from the database.

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -8,17 +8,28 @@ const galleryItems = require('../modules/gallery.data');
 router.put('/like/:id', (req, res) => {
     console.log(req.params);
     const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    const queryText = 'UPDATE gallery SET "likes" = "likes" +1 WHERE "id" = $1';
+
+    pool.query(queryText, [galleryId])
+        .then(result=>{
+            res.sendStatus(200);
+        }).catch(error=>{
+            res.sendStatus(500);
+        })
+
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+    let queryText = 'SELECT * FROM "gallery";';
+    pool.query(queryText)
+        .then(result => {
+            res.send(result.rows);
+        })
+        .catch(error =>{
+            console.log('Query:', queryText, 'WE HAD AN ERROR =>', error);
+            res.sendStatus(500);
+        })
 }); // END GET Route
 
 module.exports = router;
